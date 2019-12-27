9197335b124e598b24adf4722a278ed62d1ccd05
Quan Brew <t@ioover.net>
2019-12-27 22:06:16 +0800

    now you can configure the backend server URL at the .env file

--- .proxyrc.js
+++ .proxyrc.js
@@ -1,9 +1,15 @@
 const proxy = require('http-proxy-middleware');
+require('dotenv').config();
 
 module.exports = function(app) {
+  let backend = process.env['BACKEND_URL'];
+  if (backend === undefined) {
+    console.warn('There is not BACKEND_URL in environment variables.');
+    backend = 'http://localhost:3000/';
+  }
   app.use(
     proxy('/api', {
-      target: 'http://localhost:3000/',
+      target: backend,
     })
   );
 };
