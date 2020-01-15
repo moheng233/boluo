22f27aaa70f1655a7fdbe3e833498b426e7d928a
Quan Brew <t@ioover.net>
2020-01-15 19:22:48 +0800

    implement users APIs

---
+++ src/api/client.ts
@@ -0,0 +1,82 @@
+export interface Return<T> {
+  value: T;
+  type: 'return';
+  statusCode: number;
+}
+
+export interface Err {
+  value: string;
+  type: 'error';
+  statusCode: number;
+}
+
+export type AppResult<T> = Return<T> | Err;
+
+const csrfKey = 'csrf-token';
+
+const isTimeout = (token: string): boolean => {
+  const matched = token.match(/\.(\d+)\./);
+  if (!matched) {
+    return true;
+  }
+  const expire = parseInt(matched[1]);
+  const now = new Date().getTime() / 1000;
+  return expire < now;
+};
+
+const getCsrfToken = async (): Promise<string> => {
+  let csrfToken: string | null = localStorage.getItem(csrfKey);
+  if (csrfToken == null || isTimeout(csrfToken)) {
+    return refreshCsrfToken();
+  }
+  return csrfToken;
+};
+
+const refreshCsrfToken = async (): Promise<string> => {
+  const fetched = await fetch('/api/csrf-token', { credentials: 'include' });
+  const csrfResult: AppResult<string> = await fetched.json();
+  if (csrfResult.type == 'return') {
+    localStorage.setItem(csrfKey, csrfResult.value);
+    return csrfResult.value;
+  } else {
+    throw new Error(csrfResult.value);
+  }
+};
+
+export const request = async <T>(path: string, method: string, payload: object | null): Promise<AppResult<T>> => {
+  path = '/api' + path;
+  const csrfToken = await getCsrfToken();
+  let body: string | null;
+  if (payload !== null) {
+    body = JSON.stringify(payload);
+  } else {
+    body = null;
+  }
+  let result = await fetch(path, {
+    method,
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      'csrf-token': csrfToken,
+    }),
+    body,
+    credentials: 'include',
+  });
+  return await result.json();
+};
+
+export const post = <T>(path: string, payload: object): Promise<AppResult<T>> => request(path, 'POST', payload);
+
+type Query = { [key: string]: string | number | boolean | null };
+
+export const get = <T>(path: string, query: Query): Promise<AppResult<T>> => {
+  const parts = [];
+  for (let key in query) {
+    if (query.hasOwnProperty(key)) {
+      const value = query[key];
+      if (value !== null) {
+        parts.push(`${encodeURIComponent(key)}=${encodeURIComponent(value)}`);
+      }
+    }
+  }
+  return request(`${path}?${parts.join('&')}`, 'GET', null);
+};
---
+++ src/api/users.ts
@@ -0,0 +1,32 @@
+import { AppResult, get, post } from './client';
+
+export interface User {
+  id: string;
+  username: string;
+  nickname: string;
+  bio: string;
+  joined: string;
+  deactivated: boolean;
+  avatar_id: string | null;
+}
+
+export interface LoginResult {
+  user: User;
+  token: null;
+}
+
+export const login = (username: string, password: string): Promise<AppResult<LoginResult>> => {
+  return post<User>('/users/login', { username, password });
+};
+
+export const queryUser = (id: string): Promise<AppResult<User>> => {
+  return get('/users/query', { id });
+};
+
+export const register = (email: string, username: string, password: string, nickname: string) => {
+  return post<User>('/users/register', { email, username, password, nickname });
+};
+
+export const edit = (nickname: string, bio: string, avatar: string) => {
+  return post<User>('/users/edit', { nickname, bio, avatar });
+};
